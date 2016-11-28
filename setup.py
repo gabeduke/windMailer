51b422824840c090a407a8c7c24da4d32c313825
Dukeman <gduke@bullhorn.com>
2016-11-28 01:45:49 -0500

    refactoring

--- setup.py
+++ setup.py
@@ -5,24 +5,32 @@ setup(
     name="windMailer",
 
     # Version number (initial):
-    version="0.1.0",
+    version="0.2.6",
 
     # Application author details:
     author="dukeman",
     author_email="gabeduke@gmail.com",
 
     # Packages
-    #packages=["app"],
+    packages=["app"],
+
+    # scripts=['bin/windMailer.sh'],
 
     # Include additional files into the package
     include_package_data=True,
 
     # Details
-     url="http://pypi.python.org/pypi/windMailer_v010/",
+     url="http://pypi.python.org/pypi/windMailer_v026/",
 
     #
     # license="LICENSE.txt",
-    description="Useful towel-related stuff.",
+    description="Useful towel-related stuff. ",
+
+    # entry_points={
+    #     'console_scripts': [
+    #         'windMailer = app.__main__:main'
+    #     ]
+    # },
 
     # long_description=open("README.txt").read(),
 
