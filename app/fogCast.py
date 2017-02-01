5460bae8e0dd8a0f84982ee38ccd3887925d1844
root <root@nocheroBot.fios-router.home>
2017-02-01 14:19:22 +0000

    adding debugger

--- app/fogCast.py
+++ app/fogCast.py
@@ -2,6 +2,7 @@
 import forecastio
 import sys
 import os.path
+import pdb
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 # import sys
@@ -14,11 +15,17 @@ by_day = forecast.daily()
 by_hour = forecast.hourly()
 
 fogTimes = []
+debug = []
 
 for hourlyData in by_hour.data:
     deg = hourlyData.temperature
-    diff = abs(hourlyData.dewPoint - deg)
+    dew = hourlyData.dewPoint
+    diff = abs(dew - deg)
+    debug.append('{:%a: %I %P}'.format(hourlyData.time) + " | dewpoint = " + str(int(dew)) + " | deg = " + str(int(deg)) + " | diff = " + str(int(diff)))
     if deg <= 66 and diff <= 5:
         fogTimes.append('Chance of fog on ' + '{:%a at %I %P}'.format(hourlyData.time))
 
+pdb.set_trace()
+if len(sys.argv)>1 and "debug" in sys.argv[1:]:
+    print('\n').join(debug)
 print('\n').join(fogTimes)
