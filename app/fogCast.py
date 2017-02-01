de3d87c143c354a63d5c983082fde6f5a0e5a2cb
root <root@nocheroBot.fios-router.home>
2017-02-01 15:24:46 +0000

    fixing dew float to be integar for comparing

--- app/fogCast.py
+++ app/fogCast.py
@@ -5,8 +5,6 @@ import os.path
 import pdb
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-# import sys
-# sys.path.insert(0, "home/gabeduke/windMailer/settings.py")
 from settings import *
 
 # fetching weather
@@ -17,14 +15,18 @@ by_hour = forecast.hourly()
 fogTimes = []
 debug = []
 
+
 for hourlyData in by_hour.data:
     deg = hourlyData.temperature
     dew = hourlyData.dewPoint
     diff = abs(dew - deg)
     debug.append('{:%a: %I %P}'.format(hourlyData.time) + " | dewpoint = " + str(int(dew)) + " | deg = " + str(int(deg)) + " | diff = " + str(int(diff)))
-    if deg <= 66 and diff <= 5:
+    if deg <= 66 and int(diff) <= 5:
         fogTimes.append('Chance of fog on ' + '{:%a at %I %P}'.format(hourlyData.time))
+
 
 if len(sys.argv)>1 and "debug" in sys.argv[1:]:
     print('\n').join(debug)
+
 print('\n').join(fogTimes)
+
